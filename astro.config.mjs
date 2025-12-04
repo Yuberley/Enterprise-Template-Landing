f1369530c934c6133179ed1a516f79ddebe4e6ac
Yuberley <yuberleyguerrero1999@gmail.com>
2025-12-03 19:47:31 -0500

    Update page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://template.enterprises', // Cambia esto por tu dominio real
+	integrations: [sitemap()],
+	compressHTML: true,
+	build: {
+		inlineStylesheets: 'auto',
+	},
+});
